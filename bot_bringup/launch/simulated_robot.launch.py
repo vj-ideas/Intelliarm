dde6b24c2bbe9f9bc25981045ba133c38e1dce1f
vj-ideas <vijayanand5989@gmail.com>
2024-11-08 16:56:45 +0530

    Add files via upload

---
+++ bot_bringup/launch/simulated_robot.launch.py
@@ -0,0 +1,47 @@
+import os
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    gazebo = IncludeLaunchDescription(
+            os.path.join(
+                get_package_share_directory("armbot_cpp"),
+                "launch",
+                "gazebo.launch.py"
+            )
+        )
+    
+    controller = IncludeLaunchDescription(
+            os.path.join(
+                get_package_share_directory("bot_controller"),
+                "launch",
+                "controller.launch.py"
+            ),
+            launch_arguments={"is_sim": "True"}.items()
+        )
+    
+    moveit = IncludeLaunchDescription(
+            os.path.join(
+                get_package_share_directory("bot_moveit"),
+                "launch",
+                "moveit.launch.py"
+            ),
+            launch_arguments={"is_sim": "True"}.items()
+        )
+    
+    remote_interface = IncludeLaunchDescription(
+            os.path.join(
+                get_package_share_directory("bot_remote"),
+                "launch",
+                "remote_interface.launch.py"
+            ),
+        )
+    
+    return LaunchDescription([
+        gazebo,
+        controller,
+        moveit,
+        remote_interface,
+    ])
